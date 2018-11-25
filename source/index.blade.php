0f10677457f793c7551767dca40b4aff1d2edd7b
Patrick Barranger <patrickb@23andme.com>
2018-11-25 09:16:30 -0800

    update homepage test

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,11 +1,13 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Artisan Static for Jigsaw</h1>
+    <h1>Welcome to Patrick Barranger's Data Science Projects</h1>
 
-    <h2>Asset Compilation Test</h2>
+    <p>Over the course of my career I have worked on some fun data mining, field experiments, machine learning, and other programming challenges. I use this site to host my publicly shareable projects.</p>
+
+   <!--  <h2>Asset Compilation Test</h2>
 
     <p>CSS test: <span class="test-css">text with border and padding</span></p>
 
-    <p>JavaScript test: <a href="#" class="test-js">click me</a></p>
+    <p>JavaScript test: <a href="#" class="test-js">click me</a></p> -->
 @endsection
