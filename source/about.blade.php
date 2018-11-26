c4a0bb1903428220976df3307db579197ca235c9
Patrick Barranger <patrickb@23andme.com>
2018-11-26 10:37:01 -0800

    update about section

--- source/about.blade.php
+++ source/about.blade.php
@@ -5,7 +5,13 @@
 @section('content')
     <h1>About</h1>
 
-    <p>My name is {{ $page->owner->name }}</p>
+    <h2>Professional & Education</h2>
+
+    <p>For the past decade I have worked to distill insights from data. I began my career in academic research working in biomaterial labs to develop donor-identical tissues. In 2012 I left academia for Silicon Valley where I took a job within Operations at Google.  After 2 years I transitioned within Google to analytical consulting for Fortune 500 companies. To enhance my technical development, I attended UC Berkeley’s Masters of Information and Data Science program. I earned my degree in the Summer of 2018. Today I am a Data Scientist at 23andMe.  My work consists of using data to solve company wide business problems and managing a team of Product Analysts. </p>
+
+    <h2>Fun Stuff</h2>
+
+    <p>When I am not staring at a computer screen, I can be found racing cars, playing lacrosse, cooking, or exploring California with my wife and 2 dogs.</p>
 
     <h2>Links:</h2>
 
